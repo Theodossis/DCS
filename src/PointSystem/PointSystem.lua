fef6d4e33fa4ad80fb4580d25266a1f9a8d39625
Theodossis Papadopoulos <theodosis_pa@hotmail.com>
2020-01-18 14:36:17 +0200

    Point System Ver 1.03
    Added support for external points from other scripts

--- src/PointSystem/PointSystem.lua
+++ src/PointSystem/PointSystem.lua
@@ -1,5 +1,5 @@
 --[[
-    Point System Script - Version: 1.02 - 19/12/2019 by Theodossis Papadopoulos 
+    Point System Script - Version: 1.03 - 18/1/2020 by Theodossis Papadopoulos 
        ]]
 -- Mission Template Script already contains it
 -- Requires MIST script
@@ -12,10 +12,13 @@ local heliCost = 1 -- How many points will be removed from the team who lost the
 local shipPoints = 5 -- How many points will be rewarded to the team which sank the ship
 local unitPoints = 0.3 -- How many points will be rewarded to the team which destroyed a ground unit
 
-local printScoreEvery = 300 -- How much time between its cycle to show message (in seconds)
+local printScoreEvery = 150 -- How much time between its cycle to show message (in seconds)
 local printScoreFor = 20 -- How much time will the message show up (in seconds)
 
 -- ------------------ CODE DO NOT TOUCH ------------------
+extraBluePoints = 0
+extraRedPoints = 0
+
 local blueACCasualties = 0
 local blueHeliCasualties = 0
 local blueShipCasualties = 0
@@ -27,7 +30,7 @@ local redShipCasualties = 0
 local redGroundUnitCasualties = 0
 
 -- -------------------------EVENT LISTENER-------------------------
-POINT_SYSTEM = {}
+local POINT_SYSTEM = {}
 function POINT_SYSTEM:onEvent(event)
   local who = event.initiator
   if event.id == world.event.S_EVENT_DEAD then
@@ -62,13 +65,13 @@ function POINT_SYSTEM:onEvent(event)
 end
 world.addEventHandler(POINT_SYSTEM)
 -- -----------------------------SCORE PRINTER----------------------------------------
-function printPoints()
-  local totalPointsForBlue = redACCasualties*aircraftPoints + redHeliCasualties*heliPoints + redShipCasualties*shipPoints + redGroundUnitCasualties*unitPoints - blueACCasualties*aircraftCost - blueHeliCasualties*heliCost
-  local totalPointsForRed = blueACCasualties*aircraftPoints + blueHeliCasualties*heliPoints + blueShipCasualties*shipPoints + blueGroundUnitCasualties*unitPoints - redACCasualties*aircraftCost - redHeliCasualties*heliCost
+local function printPoints()
+  local totalPointsForBlue = redACCasualties*aircraftPoints + redHeliCasualties*heliPoints + redShipCasualties*shipPoints + redGroundUnitCasualties*unitPoints - blueACCasualties*aircraftCost - blueHeliCasualties*heliCost + extraBluePoints
+  local totalPointsForRed = blueACCasualties*aircraftPoints + blueHeliCasualties*heliPoints + blueShipCasualties*shipPoints + blueGroundUnitCasualties*unitPoints - redACCasualties*aircraftCost - redHeliCasualties*heliCost + extraRedPoints
   trigger.action.outText('###  TEAM SCORES  ###  Created by =GR= Theodossis for LoG' , printScoreFor)
   trigger.action.outText('POINTS:  BLUELAND TEAM: ' .. totalPointsForBlue .. '  / REDLAND TEAM: ' .. totalPointsForRed, printScoreFor)
-  trigger.action.outText('BLUE TEAM CASUALTIES: A/C:' .. blueACCasualties .. ' HELO: ' .. blueHeliCasualties .. ' SHIPS: ' .. blueShipCasualties .. ' GROUND UNITS: ' .. blueGroundUnitCasualties, printScoreFor)  
-  trigger.action.outText('RED TEAM CASUALTIES: A/C:' .. redACCasualties .. ' HELO: ' .. redHeliCasualties .. ' SHIPS: ' .. redShipCasualties .. ' GROUND UNITS: ' .. redGroundUnitCasualties, printScoreFor)  
+  trigger.action.outText('BLUE TEAM CASUALTIES: A/C:' .. blueACCasualties .. ' HELO: ' .. blueHeliCasualties .. ' SHIPS: ' .. blueShipCasualties .. ' GROUND UNITS: ' .. blueGroundUnitCasualties .. '\nPoints from missions: ' .. extraBluePoints, printScoreFor)  
+  trigger.action.outText('RED TEAM CASUALTIES: A/C:' .. redACCasualties .. ' HELO: ' .. redHeliCasualties .. ' SHIPS: ' .. redShipCasualties .. ' GROUND UNITS: ' .. redGroundUnitCasualties .. '\nPoints from missions: ' .. extraRedPoints, printScoreFor)  
 end
 
 mist.scheduleFunction(printPoints, nil, timer.getTime() + 10, printScoreEvery)
