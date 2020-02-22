3b62807ba5c8e5b0cc724fad472944c1b7e42944
Theodossis Papadopoulos <theodosis_pa@hotmail.com>
2020-02-22 16:43:01 +0200

    MoveCargo Ver. 1.1
    Added menu that shows progression

--- src/MoveCargo.lua
+++ src/MoveCargo.lua
@@ -1,15 +1,35 @@
 --[[
-    MoveCargo Script - Version: 1.0 - 21/2/2020 by Theodossis Papadopoulos
+    MoveCargo Script - Version: 1.1 - 21/2/2020 by Theodossis Papadopoulos
     -- Requires MIST
     -- With love for =GR= Spanker
-    -- DON'T FORGET TO CREATE A SPAWN ZONE
+    -- DON'T FORGET TO CREATE A SPAWN_ZONE
        ]]
 
 local CARGO_SCRIPT = {}
-
 -- ---------------------------CONFIGURATION---------------------------
 local NOT_IN_ZONE_MESSAGE = "You must be inside the spawn zone to spawn a crate!"
 local CRATE_MASS = 300
+local SHAPE_NAME = "ab-212_cargo" -- MIKRO DYXTIOTO
+local TYPE = "uh1h_cargo"
+
+--[[ Placeholder for different crates 
+
+local SHAPE_NAME = "bw_container_cargo" -- MEGALO PRASINO
+local TYPE = "container_cargo"
+
+local SHAPE_NAME = "ab-212_cargo" -- MIKRO DYXTIOTO
+local TYPE = "uh1h_cargo"
+
+local SHAPE_NAME = "bw_container_cargo"
+local TYPE = "container_cargo"
+
+local SHAPE_NAME = "f_bar_cargo"
+local TYPE = "f_bar_cargo"
+
+local SHAPE_NAME = "fueltank_cargo"
+local TYPE = "fueltank_cargo"
+
+]]
 
 CARGO_SCRIPT[1] = {
   ZONE_NAME = "ZONE_1",
@@ -23,7 +43,6 @@ CARGO_SCRIPT[2] = {
   FLAG_NAME = 2,
   SET_TO = 5,
 }
-
 -- ---------------------------CODE DO NOT TOUCH---------------------------
 
 local function tablelength(T)
@@ -64,17 +83,42 @@ local function checker()
     local all_cargos = tableConcatNew(blue_cargos, red_cargos)
     for j=1, tablelength(all_cargos) do
       local cargo = all_cargos[j]
-      if (isInZone(cargo:getPosition().p, zone)) and (cargo:inAir() == false) and (cargo:getLife() >= 1) then
+      if (isInZone(cargo:getPosition().p, zone)) and (cargo:inAir() == false) and (cargo:getLife() >= 1) and (cargo:getTypeName() == TYPE) then
         cnt = cnt + 1
       end
     end
     if cnt == script.HOW_MANY then
-      trigger.action.outText("SUCCESS " .. script.ZONE_NAME, 10) -- DEBUG
+      -- trigger.action.outText("SUCCESS " .. script.ZONE_NAME, 10) -- DEBUG
       trigger.action.setUserFlag(script.FLAG_NAME, script.SET_TO)
       table.remove(CARGO_SCRIPT, i)
       break
     end
   end
+end
+
+local function showProgress(gpid)
+  local finalText = "Crate Transfer Progression: "
+  local textEmpty = true
+  for i=1, tablelength(CARGO_SCRIPT) do
+    local cnt = 0
+    local script = CARGO_SCRIPT[i]
+    local zone = trigger.misc.getZone(script.ZONE_NAME)
+    local blue_cargos = coalition.getStaticObjects(coalition.side.BLUE)
+    local red_cargos = coalition.getStaticObjects(coalition.side.RED)
+    local all_cargos = tableConcatNew(blue_cargos, red_cargos)
+    for j=1, tablelength(all_cargos) do
+      local cargo = all_cargos[j]
+      if (isInZone(cargo:getPosition().p, zone)) and (cargo:inAir() == false) and (cargo:getLife() >= 1) and (cargo:getTypeName() == TYPE) then
+        cnt = cnt + 1
+      end
+    end
+    textEmpty = false
+    finalText = finalText .. "\n- " .. script.ZONE_NAME .. " " .. cnt .. "/" .. script.HOW_MANY
+  end
+  if textEmpty == true then
+    finalText = finalText .. "COMPLETE"
+  end
+  trigger.action.outTextForGroup(gpid, finalText, 15)
 end
 
 local function getFront(position, offset)
@@ -99,26 +143,8 @@ local function spawnCrate(gpid)
         local front = getFront(position, 20)
         local crate = {
           ["category"] = "Cargos",
-          ["shape_name"] = "ab-212_cargo", -- MIKRO DYXTIOTO
-          ["type"] = "uh1h_cargo",
-          --[[ Placeholder for different crates 
-          
-          ["shape_name"] = "bw_container_cargo", -- MEGALO PRASINO
-          ["type"] = "container_cargo",
-          
-          ["shape_name"] = "ab-212_cargo", -- MIKRO DYXTIOTO
-          ["type"] = "uh1h_cargo",
-          
-          ["shape_name"] = "bw_container_cargo",
-          ["type"] = "container_cargo",
-          
-          ["shape_name"] = "f_bar_cargo",
-          ["type"] = "f_bar_cargo",
-          
-          ["shape_name"] = "fueltank_cargo",
-          ["type"] = "fueltank_cargo",
-          
-          ]]
+          ["shape_name"] = SHAPE_NAME, 
+          ["type"] = TYPE,
           ["y"] = front.z,
           ["x"] = front.x,
           ["mass"] = CRATE_MASS,
@@ -145,7 +171,9 @@ function EV_MANAGER:onEvent(event)
       if event.initiator:getGroup():getCategory() == Group.Category.HELICOPTER then
         local gpid = event.initiator:getGroup():getID()
         missionCommands.removeItemForGroup(gpid, {[1] = "Spawn crate"})
+        missionCommands.removeItemForGroup(gpid, {[1] = "Show crate progress"})
         missionCommands.addCommandForGroup(gpid, "Spawn crate", nil, spawnCrate, gpid)
+        missionCommands.addCommandForGroup(gpid, "Show crate progress", nil, showProgress, gpid)
       end
     end
   end
